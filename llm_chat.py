2d04a61fc2faa75dd8f4633f0bc603c1ceecd9a7
Ishani Udeshika <ishaniu.udeshika@gmail.com>
2025-06-18 00:25:14 +0530

    Update llm_chat.py

--- llm_chat.py
+++ llm_chat.py
@@ -2,7 +2,7 @@ import streamlit as st
 from llama_index.llms.groq import Groq
 
 # Set your Groq API Key
-GROQ_API_KEY = "your groq_api_key_here"
+GROQ_API_KEY = "YOUR_API_KEY_HERE"
 
 # Initialize the LLM
 llm = Groq(model="llama3-8b-8192", api_key=GROQ_API_KEY)
